d7c53d373e3cb6a7715aa1dd221b999695ebe40d
majoferenc <44170671+majoferenc@users.noreply.github.com>
2024-04-29 09:09:06 +0200

    Create streamlit_app_v2.py

---
+++ streamlit_app_v2.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from langchain_community.llms import Ollama
+import os
+
+# App title
+st.set_page_config(page_title="🦙💬 Local Llama 3 Chatbot")
+
+with st.sidebar:
+    st.title('🦙💬 Llama 3 Chatbot')
+    st.subheader('Models and parameters')
+    llm = Ollama(model="llama3:latest") # 👈 stef default
+   
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+# Function for generating LLaMA2 response
+def generate_llama2_response(prompt_input):
+    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    output = st.write_stream(llm.stream(prompt, stop=['<|eot_id|>']))
+    return output
+
+# User-provided prompt
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = llm.stream(prompt, stop=['<|eot_id|>'])
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
